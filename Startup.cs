08cb4a74ac1b209ec0420aacd01020edf3607336
micheledelloro <md562775@gmail.com>
2020-04-11 23:01:48 +0200

    Secondo Commit

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace MyCourse
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!!!");
                 });
             });
         }
